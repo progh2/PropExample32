ca341032b12ec127bff59e3339c2b3b6baade801
Gihun Ham <progh2@gmail.com>
2025-06-04 14:10:57 +0900

    피보나치 재귀 함수

--- Program.cs
+++ Program.cs
@@ -73,6 +73,12 @@ namespace PropExample32
             testA.value = 10;
             testB.value = 20;
             Console.WriteLine(testA.value);
+
+            // 피보나치 재귀 함수
+            Fibonacci fibo = new Fibonacci();
+            Console.WriteLine(fibo.Get(1));
+            Console.WriteLine(fibo.Get(100));
+
         }
 
         class Test
